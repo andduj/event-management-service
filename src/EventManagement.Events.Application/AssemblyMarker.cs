0ea1d60632f7473e8d0346eecc4bcd85c71d49ad
Дуюнов Андрей Сергеевич <Duyunov.AS@gazprom-neft.ru>
2026-06-01 12:51:43 +0300

    Добавлены проекты Domain, Application, Infrastructure

---
+++ src/EventManagement.Events.Application/AssemblyMarker.cs
@@ -0,0 +1,6 @@
+namespace EventManagement.Events.Application;
+
+/// <summary>
+/// Маркер сборки Application. Use cases будут перенесены на следующих этапах.
+/// </summary>
+internal static class AssemblyMarker;
---
+++ src/EventManagement.Events.Domain/AssemblyMarker.cs
@@ -0,0 +1,6 @@
+namespace EventManagement.Events.Domain;
+
+/// <summary>
+/// Маркер сборки Domain. Доменные типы будут перенесены на следующих этапах.
+/// </summary>
+internal static class AssemblyMarker;
